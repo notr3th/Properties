01f8eb5aaef402fa35963407fcc7e383bf538cfd
notr3th <notr3th@gmail.com>
2025-05-08 13:30:55 +0800

    Create CanvasGroup.lua

---
+++ CanvasGroup.lua
@@ -0,0 +1,27 @@
+CanvasGroup = {
+    Order = {"Appearance","Data","Localization","Selection","State"},
+
+    Appearance = {
+        "GroupColor3","GroupTransparency","SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","Interactable","LayoutOrder","Name","Parent","Position",
+        "Rotation","Size","SizeConstraint","UniqueId","Visible","ZIndex"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionGroup","SelectionOrder"
+    },
+
+    State = {
+        "GuiState"
+    }
+},
