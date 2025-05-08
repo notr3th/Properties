e1431dc7c36d2a571559a9ad1f38ac01e6a7fbe4
notr3th <notr3th@gmail.com>
2025-05-08 13:41:58 +0800

    Create UIListLayout.lua

---
+++ UIListLayout.lua
@@ -0,0 +1,21 @@
+UIListLayout = {
+    Order = {"Appearance","Data","Behavior","Alignment"},
+
+    Appearance = {
+        "Padding"
+    },
+
+    Data = {
+        "Archivable","AbsoluteContentSize","ClassName","Name","Parent","UniqueId"
+    },
+    
+    Behavior = {
+        "FillDirection","SortOrder","Wraps"
+    },
+    
+    Alignment = {
+        "HorizontalAlignment","HorizontalFlex","ItemLineAlignment","VerticalAlignment","VerticalFlex"
+    }
+},
+
+return UIListLayout
