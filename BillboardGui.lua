270d2d1a41eb5175946aaf8be550841392380653
notr3th <notr3th@gmail.com>
2025-05-08 13:30:22 +0800

    Create BillboardGui.lua

---
+++ BillboardGui.lua
@@ -0,0 +1,22 @@
+BillboardGui = {
+    Order = {"Data","Behavior","Localization","Selection"},
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","Adornee",
+        "AlwaysOnTop","ClassName","Enabled","ExtentsOffset","ExtentsOffsetWorldSpace","LightInfluence",
+        "MaxDistance","Name","Parent","PlayerToHideFrom","ResetOnSpawn","Size","SizeOffset",
+        "StudsOffset","StudsOffsetWorldSpace","UniqueId","ZIndexBehavior"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "SelectionGroup"
+    }
+},
