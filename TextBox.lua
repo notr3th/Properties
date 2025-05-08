db522cf3b3aea7c587075883bcb1c02e4dc0a308
notr3th <notr3th@gmail.com>
2025-05-08 13:37:05 +0800

    Create TextBox.lua

---
+++ TextBox.lua
@@ -0,0 +1,41 @@
+TextBox = {
+    Order = {"Appearance","Data","Behavior","Localization","Selection","State","Text"},
+
+    Appearance = {
+        "SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","ClearTextOnFocus","CursorPosition","Interactable","LayoutOrder",
+        "MultiLine","Name","Parent","Position","Rotation","SelectionStart","ShowNativeInput","Size",
+        "SizeConstraint","TextEditable","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionGroup","SelectionOrder"
+    },
+
+    State = {
+        "GuiState"
+    },
+
+    Text = {
+        "ContentText","FontFace","LineHeight","MaxVisbleGraphemes","OnlyTypeFeatures","PlaceholderColor3",
+        "PlaceholderText","RichText","Text","TextBounds","TextColor3","TextDirection","TextFits","TextScaled",
+        "TextSize","TextStrokeColor3","TextStrokeTransparency","TextTransparency","TextTruncate","TextWrapped",
+        "TextXAlignment","TextYAlignment"
+    }
+},
+
+return TextBox
