aee12b734f87efe6cab22ddec5b916bf6ae23bb3
notr3th <notr3th@gmail.com>
2025-05-08 13:34:46 +0800

    Create Path2D.lua

---
+++ Path2D.lua
@@ -0,0 +1,14 @@
+Path2D = {
+    Order = {"Data","ControlPoints"},
+
+    Data = {
+        "Archivable","ClassName","Closed","Color3","Name","Parent","Thickness","UniqueId","Visible",
+        "ZIndex"
+    },
+
+    ControlPoints = {
+        "SelectedControlPoint","SelectedControlPointData"
+    }
+},
+
+return Path2D
