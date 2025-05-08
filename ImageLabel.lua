5affe4d800aa457baf91f1323300884cba4a8f66
notr3th <notr3th@gmail.com>
2025-05-08 13:34:18 +0800

    Create ImageLabel.lua

---
+++ ImageLabel.lua
@@ -0,0 +1,38 @@
+ImageLabel = {
+    Order = {"Appearance","Data","Behavior","Haptic","Image","Localization","Selection","State"},
+
+    Appearance = {
+        "SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","Interactable","LayoutOrder","Name","Parent","Position",
+        "Rotation","Size","SizeConstraint","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Image = {
+        "Image","ImageColor3","ImageRectOffset","ImageRectSize","ImageTransparency","IsLoaded",
+        "ResampleMode","ScaleType"
+    },
+    
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+    
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionGroup","SelectionOrder"
+    },
+
+    State = {
+        "GuiState"
+    }
+},
+
+return ImageLabel
