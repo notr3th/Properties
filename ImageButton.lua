21af4b2f389add60b2d6e3b8fadee94fefbbc437
notr3th <notr3th@gmail.com>
2025-05-08 13:32:35 +0800

    Create ImageButton.lua

---
+++ ImageButton.lua
@@ -0,0 +1,41 @@
+ImageButton = {
+    Order = {"Appearance","Data","Behavior","Haptic","Image","Localization","Selection","State"},
+
+    Appearance = {
+        "SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint","AutoButtonColor",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","Interactable","LayoutOrder","Modal","Name","Parent","Position",
+        "Rotation","Size","SizeConstraint","Style","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+    Haptic = {
+        "HoverHapticEffect","PressHapticEffect"
+    },
+
+    Image = {
+        "HoverImage","Image","ImageColor3","ImageRectOffset","ImageRectSize","ImageTransparency",
+        "IsLoaded","PressedImage","ResampleMode","ScaleType"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "Selected","SelectionGroup","SelectionOrder"
+    },
+
+    State = {
+        "GuiState"
+    }
+},
+
+return ImageButton
