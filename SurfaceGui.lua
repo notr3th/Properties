abaf3127e377cd6070640311d5c132426f37f8f3
notr3th <notr3th@gmail.com>
2025-05-08 13:36:24 +0800

    Create SurfaceGui.lua

---
+++ SurfaceGui.lua
@@ -0,0 +1,31 @@
+SurfaceGui = {
+    Order = {"Appearance","Data","Behavior","Localization","Selection","Sizing"},
+
+    Appearance = {
+        "ZOffset"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","Adornee",
+        "AlwaysOnTop","ClassName","Enabled","Face","LightInfluence","MaxDistance","Name",
+        "Parent","ResetOnSpawn","ToolPunchThroughDistance","UniqueId","ZIndexBehavior"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "SelectionGroup"
+    },
+
+    Sizing = {
+        "PixelsPerStud","SizingMode"
+    }
+},
+
+return SurfaceGui
