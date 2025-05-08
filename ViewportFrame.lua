be01de20edb20a00145e0ed5493ea50ca4d3e220
notr3th <notr3th@gmail.com>
2025-05-08 13:45:59 +0800

    Create ViewportFrame.lua

---
+++ ViewportFrame.lua
@@ -0,0 +1,33 @@
+ViewportFrame = {
+    Order = {"Appearance","Data","Behavior","Image","Localization","Selection"},
+    
+    Appearance = {
+        "Ambient","LightColor","LightDirection","SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","CurrentCamera","Interactable","LayoutOrder","Name","Parent",
+        "Position","Rotation","Size","SizeConstraint","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Image = {
+        "ImageColor3","ImageTransparency"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+    
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionGroup","SelectionOrder"
+    }
+}
+
+return ViewportFrame
