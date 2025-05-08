25e0b90544ad69aec76afdf31bab0e2e19ee5f62
notr3th <notr3th@gmail.com>
2025-05-08 13:35:53 +0800

    Create ScrollingFrame.lua

---
+++ ScrollingFrame.lua
@@ -0,0 +1,41 @@
+ScrollingFrame = {
+    Order = {"Appearance","Data","Behavior","Localization","Scrolling","Selection","State"},
+
+    Appearance = {
+        "SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","Interactable","LayoutOrder","Name","Parent","Position",
+        "Rotation","Size","SizeConstraint","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Scrolling = {
+        "AbsoluteCanvasSize","AbsoluteWindowSize","AutomaticCanvasSize","BottomImage","CanvasPosition",
+        "CanvasSize","ElasticBehavior","HorizontalScrollBarlnset","MidImage","ScrollBarImageColor3",
+        "ScrollBarImageTransparency","ScrollBarThickness","ScrollingDirection","ScrollingEnabled",
+        "TopImage","VerticalScrollBarlnset","VerticalScrollBarPosition"
+    },
+
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionBehaviorDown","SelectionBehaviorLeft","SelectionBehaviorRight",
+        "SelectionBehaviorUp","SelectionGroup","SelectionOrder"
+    },
+    
+    State = {
+        "GuiState"
+    }
+},
+
+return ScrollingFrame
