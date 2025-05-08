32dabb2642064a64ac1030b4049932165c57a7eb
notr3th <notr3th@gmail.com>
2025-05-08 13:43:26 +0800

    Create UIScale.lua

---
+++ UIScale.lua
@@ -0,0 +1,13 @@
+UIScale = {
+    Order = {"Data","Behavior"},
+
+    Data = {
+        "Archivable","ClassName","Name","Parent","UniqueId"
+    },
+    
+    Behavior = {
+        "Scale"
+    }
+},
+
+return UIScale
