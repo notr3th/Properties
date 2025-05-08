8e13dc29ee2418e9f1ab4d33a1198e847cc05f6c
notr3th <notr3th@gmail.com>
2025-05-08 13:44:18 +0800

    Create UIStroke.lua

---
+++ UIStroke.lua
@@ -0,0 +1,13 @@
+UIStroke = {
+    Order = {"Appearance","Data"},
+
+    Appearance = {
+        "ApplyStrokeMode","Color","LineJoinMode","Thickness","Transparency"
+    },
+
+    Data = {
+        "Archivable","ClassName","Enabled","Name","Parent","UniqueId"
+    }
+},
+
+return UIStroke
