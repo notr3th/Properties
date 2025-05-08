384b2e20d583feb0d774f2774059e7b0c00a0be7
notr3th <notr3th@gmail.com>
2025-05-08 13:41:14 +0800

    Create UIGridLayout.lua

---
+++ UIGridLayout.lua
@@ -0,0 +1,22 @@
+UIGridLayout = {
+    Order = {"Appearance","Data","Behavior","Alignment"},
+
+    Appearance = {
+        "CellPadding","CellSize"
+    },
+
+    Data = {
+        "Archivable","AbsoluteCellCount","AbsoluteCellSize","AbsoluteContentSize","ClassName",
+        "Name","Parent","UniqueId"
+    },
+
+    Behavior = {
+        "FillDirection","FillDirectionMaxCells","SortOrder","StartCorner"
+    },
+    
+    Alignment = {
+        "HorizontalAlignment","VerticalAlignment"
+    }
+},
+
+return UIGridLayout
