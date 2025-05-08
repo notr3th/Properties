859a145c4087a954a128ac6b15eb73e70791e557
notr3th <notr3th@gmail.com>
2025-05-08 13:42:58 +0800

    Create UIPageLayout.lua

---
+++ UIPageLayout.lua
@@ -0,0 +1,25 @@
+UIPageLayout = {
+    Order = {"Appearance","Data","Behavior","Alignment","Input"},
+
+    Appearance = {
+        "Animated","Circular","EasingDirection","EasingStyle","Padding","TweenTime"
+    },
+
+    Data = {
+        "Archivable","AbsoluteContentSize","ClassName","CurrentPage","Name","Parent","UniqueId"
+    },
+
+    Behavior = {
+        "FillDirection","SortOrder"
+    },
+    
+    Alignment = {
+        "HorizontalAlignment","VerticalAlignment"
+    },
+    
+    Input = {
+        "GamepadInputEnabled","ScrollWheelInputEnabled","TouchInputEnabled"
+    }
+},
+
+return UIPageLayout
