f1137c5e519ff2b7250dcd49d7363cd362a25764
notr3th <notr3th@gmail.com>
2025-05-08 13:35:13 +0800

    Create ScreenGui.lua

---
+++ ScreenGui.lua
@@ -0,0 +1,22 @@
+ScreenGui = {
+    Order = {"Appearance","Data","Localization","Selection"},
+
+    Appearance = {
+        "ClipToDeviceSafeArea","SafeAreaCompatibility","ScreenInsets"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","ClassName","DisplayOrder",
+        "Enabled","IgnoreGuilnset","Name","Parent","ResetOnSpawn","UniqueId","ZIndexBehavior"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+    
+    Selection = {
+        "SelectionGroup"
+    },
+},
+
+return ScreenGui
