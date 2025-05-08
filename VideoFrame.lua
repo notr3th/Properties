c9a34b6e50ef2b55efc664d3e614f60816696e80
notr3th <notr3th@gmail.com>
2025-05-08 13:45:36 +0800

    Create VideoFrame.lua

---
+++ VideoFrame.lua
@@ -0,0 +1,37 @@
+VideoFrame = {
+    Order = {"Appearance","Data","Behavior","Localization","Selection","State","Video"},
+
+    Appearance = {
+        "SelectionImageObject"
+    },
+
+    Data = {
+        "Archivable","AbsolutePosition","AbsoluteRotation","AbsoluteSize","Active","AnchorPoint",
+        "AutomaticSize","BackgroundColor3","BackgroundTransparency","BorderColor3","BorderMode",
+        "BorderSizePixel","ClassName","Interactable","LayoutOrder","Name","Parent","Position",
+        "Rotation","Size","SizeConstraint","UniqueId","Visible","ZIndex"
+    },
+
+    Behavior = {
+        "ClipsDescendants"
+    },
+
+    Localization = {
+        "AutoLocalize","RootLocalizationTable"
+    },
+
+    Selection = {
+        "NextSelectionDown","NextSelectionLeft","SelectionRight","SelectionUp","Selectable",
+        "SelectionGroup","SelectionOrder"
+    },
+
+    State = {
+        "GuiState"
+    },
+    
+    Video = {
+        "IsLoaded","Looped","Playing","Resolution","TimeLength","TimePosition","Video","Volume"
+    }
+},
+
+return VideoFrame
