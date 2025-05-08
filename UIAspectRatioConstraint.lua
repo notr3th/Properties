1bd47d22702b54a55f5ef9a1e563e22d2b98e9d3
notr3th <notr3th@gmail.com>
2025-05-08 13:38:42 +0800

    Create UIAspectRatioConstraint.lua

---
+++ UIAspectRatioConstraint.lua
@@ -0,0 +1,13 @@
+UIAspectRatioConstraint = {
+    Order = {"Data","Behavior"},
+
+    Data = {
+        "Archivable","ClassName","Name","Parent","UniqueId"
+    },
+    
+    Behavior = {
+        "AspectRatio","AspectType","DominantAxis"
+    }
+},
+
+return UIAspectRatioConstraint
